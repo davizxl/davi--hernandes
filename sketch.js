dddfa80df36dcafe009070c65e219b3938f121a6
davizxl <davihernandescastilho@gmail.com>
2025-10-20 21:15:02 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,117 +1,36 @@
-let cobraTamanho = 25;
-let posicaoX = 400 - cobraTamanho/2;
-let posicaoY = 250 - cobraTamanho/2;
-let direcao = 'direita';
-let comidaX;
-let comidaY;
-let larguraTela = 800;
-let alturaTela = 500;
-let cobraX = [];
-let cobraY = [];
-let pontos = 3;
 let frameRateJogo = 5;
+let cobra;
+let comida;
 
 function setup() {
-  createCanvas(larguraTela, alturaTela);
-  atualizarPosicaoComida();
-  for(let i = 0; i < pontos; i++) {
-    cobraX.push(posicaoX - i * cobraTamanho);
-    cobraY.push(posicaoY);
-  }
+  createCanvas(400, 400);
+  cobra = new Cobra(20);
+  comida = new Comida();
+  comida.criar(width, height);
 }
 
 function draw() {
   frameRate(frameRateJogo);
-  background (220); // cinza claro
+  background(220);
 
-  if (encostouNasBordasDaTela()) {
-    noLoop();
-  }
-  desenharCobra();
-  desenharComida(comidaX, comidaY, 20);
-  mudarDirecao();
-  movimentarCobra();
-}
+  cobra.mover();
+  cobra.desenhar();
 
-function movimentarCobra() {
-  if (direcao == 'direita') {
-    posicaoX = posicaoX + 5;
-  }
-  if(direcao == 'esquerda') {
-    posicaoX = posicaoX - 5;
-  }
-  if(direcao == 'cima') {
-    posicaoY = posicaoY - 5;
-  }
-  if(direcao == 'baixo') {
-    posicaoY = posicaoY + 5;
-  } 
-
-  let cabecaX = cobraX[0];
-  let cabecaY = cobraY[0];
-
-  if(direcao == 'direita') {
-    cabecaX = cabecaX + cobraTamanho;
+  if (cobra.comer(comida.x, comida.y)) {
+    frameRateJogo += 2;
+    comida.criar(width, height);
   }
 
-  if(direcao == 'esquerda') {
-    cabecaX = cabecaX - cobraTamanho
-  }
+  comida.desenhar();
 
-  if(direcao == 'cima') {
-    cabecaY = cabecaY - cobraTamanho
-  }
-
-  if(direcao == 'baixo') {
-    cabecaY = cabecaY + cobraTamanho
-  }
-
-  cobraX.unshift(cabecaX);
-  cobraY.unshift(cabecaY);
-
-  //remover último valor da lista
-
-  cobraX.pop();
-  cobraY.pop();
-}
-
-function mudarDirecao() {
-  if (keyIsDown(RIGHT_ARROW)) {
-    direcao = 'direita';
-  }
-  if (keyIsDown(LEFT_ARROW)) {
-    direcao =  'esquerda';
-  }
-  if (keyIsDown(UP_ARROW)) {
-    direcao =  'cima';
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    direcao =  'baixo';
+  if (cobra.verificarColisao()) {
+    gameOver();
   }
 }
 
-function atualizarPosicaoComida() {
-  comidaX = floor(random(0, 780));
-  comidaY = floor(random(0, 480));
-}
-
-function desenharComida(posicaoX, posicaoY, tamanho) {
-  rect(posicaoX, posicaoY, tamanho, tamanho);
-}
-
-function encostouNasBordasDaTela() {
-  let cabecaX = cobraX[0];
-  let cabecaY = cobraY[0];
-
-
-  if (cabecaX < 0 || cabecaX > larguraTela - cobraTamanho || posicaoY < 0 || posicaoY > alturaTela - cobraTamanho) {
-    return true;
-  }
-  return false;
-}
-
-function desenharCobra() {
-  for (let i = 0; i < cobraX.length; i++) {
-    rect(cobraX[i], cobraY[i], cobraTamanho, cobraTamanho);
-  }
+function gameOver() {
+  noLoop();
+  fill(0);
+  textSize(24);
+  text("Game Over!", 140, 200);
 }
